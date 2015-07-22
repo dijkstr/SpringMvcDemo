97c9cf1dc7346a1f7bb6494c3bf534f2b636716d
wanmghailong <924389979@qq.com>
2015-07-22 22:12:01 +0800

    quartz-mysql

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,6 @@
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.util.Date;
-
 /**
  * Created by wanghailong on 15-7-22.
  * hello quartz!
@@ -13,14 +11,16 @@ public class App {
         try
         {
             JobDetail jobdetail = new JobDetail("job1_1", "jgroup1", SimpleJob.class);
-            SimpleTrigger simpleTrigger = new SimpleTrigger("trigger1", "tgroup1");
-            simpleTrigger.setStartTime(new Date());
-            simpleTrigger.setRepeatInterval(1000);
-            simpleTrigger.setRepeatCount(4);
+//            SimpleTrigger simpleTrigger = new SimpleTrigger("trigger1", "tgroup1");
+//            simpleTrigger.setStartTime(new java.util.Date());
+//            simpleTrigger.setRepeatInterval(1000);
+//            simpleTrigger.setRepeatCount(4);
+
+            CronTrigger trigger = new CronTrigger("trigger1","jgroup1","0/1 * * * * ?");
 
             SchedulerFactory schedulerFactory = new StdSchedulerFactory() ;
             Scheduler scheduler = schedulerFactory.getScheduler();
-            scheduler.scheduleJob(jobdetail, simpleTrigger);
+            scheduler.scheduleJob(jobdetail, trigger);
             scheduler.start();
 
         }catch (Exception ex)
--- src/main/java/SimpleJob.java
+++ src/main/java/SimpleJob.java
@@ -2,6 +2,7 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.sql.Connection;
 import java.util.Date;
 
 /**
@@ -9,9 +10,14 @@ import java.util.Date;
  */
 
 public class SimpleJob implements Job {
+    private static int n = 0;
+    private static final String findSql_src = "select sale_begin from item_db.item";
     public void execute(JobExecutionContext jobCtx)throws JobExecutionException
     {
-        System.out.println(jobCtx.getTrigger().getClass().getName()+" triggered time is: "+new Date());
+//        Connection con_select = new
+        System.out.println("DB run in  " + jobCtx.getTrigger().getClass().getName() + " triggered time is: " + new Date() + " " + ++n);
+        if(n == 9)
+            System.out.println(">>>>>>>>>>>>>>>>>");
     }
 
 }
